06404e90770c881ed1ae660ce32f77f98fbc5315
gsp <gsp@student.dei.uc.pt>
2014-10-31 19:05:23 +0000

    URL Validation 91%

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -8,7 +9,15 @@ public class Main {
 	public static boolean DEBUG = true;
 
 	public static void main(String args[]) {
-
+//		Elimina ficheiros .part
+		File folder = new File(".");
+		File[] listOfFiles = folder.listFiles();
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].toString().endsWith(".part"))
+			{
+				listOfFiles[i].delete();
+			}
+		}
 		
 //		String youtubeUrl = args[0];
 //		String youtubeUrl = Read.readString("Introduza o link do YouTube: ");
@@ -16,64 +25,69 @@ public class Main {
 //		String youtubeUrl = new String("http://www.youtube.com/watch?v=jK88pRSakms&list=PL7FDC95FD05B2EDAF");
 
 		String url1 = new String("https://www.youtube.com/watch?v=1YWDLjvfEs4");
-		String url2 = new String("https://www.youtube.com/watch?v=jJT0Suanqhg");
+		String url2 = new String("http://youtu.be/jJT0Suanqhg?t=37s");
 		String url3 = new String("http://www.youtube.com/watch?v=Iv-Xmv2yjjQ");
 		String url4 = new String("http://www.youtube.com/watch?v=jK88pRSakms&list=RDjK88pRSakms#t=168");
-//		String youtubeUrl = url1;
-//		String youtubeUrl = url2;
-//		String youtubeUrl = url3;
-		String youtubeUrl = url4;
-		
-//		System.out.println(youtubeUrl.startsWith("https://www.youtube.com/watch?v="));
-//		System.out.println(youtubeUrl.startsWith("http://www.youtube.com/watch?v="));
-//		System.out.println(youtubeUrl.startsWith("www.youtube.com/watch?v="));
-//		System.out.println(youtubeUrl.startsWith("youtube.com/watch?v="));
-//		System.out.println(youtubeUrl.startsWith("https://www.youtube.com/v/"));
-//		System.out.println(youtubeUrl.startsWith("http://www.youtube.com/v/"));
-//		System.out.println(youtubeUrl.startsWith("www.youtube.com/v/"));
-//		System.out.println(youtubeUrl.startsWith("youtube.com/v/"));
+//		String url2 = new String("https://www.youtube.com/watch?v=jJT0Suanqhg");
 
-		
+		doWork(url1);
+		doWork(url2);
+		doWork(url3);
+		doWork(url4);
+//		doWork(url5);
+	}
+	
+	public static void doWork(String youtubeUrl)
+	{
+		String id = new String();
 //		Verifica se o URL e valido
-		if (youtubeUrl.startsWith("https://www.youtube.com/watch?v=") || 
-			youtubeUrl.startsWith("http://www.youtube.com/watch?v=") || 
-			youtubeUrl.startsWith("www.youtube.com/watch?v=") || 
-			youtubeUrl.startsWith("youtube.com/watch?v=") ||
-			youtubeUrl.startsWith("http://youtube.com/watch?v=") ||
-			youtubeUrl.startsWith("https://youtube.com/watch?v=") ||
-			
-			youtubeUrl.startsWith("https://www.youtube.com/v/") ||
-			youtubeUrl.startsWith("http://www.youtube.com/v/") ||
-			youtubeUrl.startsWith("www.youtube.com/v/") || 
-			youtubeUrl.startsWith("youtube.com/v/") ||			
-			youtubeUrl.startsWith("http://www.youtube.com/v/") || 
-			youtubeUrl.startsWith("https://youtube.com/v/") ||
-			
-			youtubeUrl.startsWith("https://youtu.be/") ||
-			youtubeUrl.startsWith("http://youtu.be/") ||
-			youtubeUrl.startsWith("youtu.be/"))
+		if (youtubeUrl.startsWith("https://www.youtube.com/watch?v=") 
+				|| youtubeUrl.startsWith("http://www.youtube.com/watch?v=") 
+				|| youtubeUrl.startsWith("www.youtube.com/watch?v=") 
+				|| youtubeUrl.startsWith("youtube.com/watch?v=")
+				|| youtubeUrl.startsWith("http://youtube.com/watch?v=")
+				|| youtubeUrl.startsWith("https://youtube.com/watch?v=") 
+							
+				|| youtubeUrl.startsWith("https://www.youtube.com/v/")
+				|| youtubeUrl.startsWith("http://www.youtube.com/v/")
+				|| youtubeUrl.startsWith("www.youtube.com/v/") 
+				|| youtubeUrl.startsWith("youtube.com/v/") 
+				|| youtubeUrl.startsWith("http://youtube.com/v/") 
+				|| youtubeUrl.startsWith("https://youtube.com/v/")
+
+				|| youtubeUrl.startsWith("https://youtu.be/") 
+				|| youtubeUrl.startsWith("http://youtu.be/") 
+				|| youtubeUrl.startsWith("youtu.be/"))
 		{
 			
 			
 //			Verifica se e uma lista e retira so o id do primeiro
-			debug("lista");
+			debug("Verifica se e lista");
 			if (youtubeUrl.contains("&list="))
 			{
 				System.out.println("Erro: link de uma lista, so vai ser transferido o video atual!");
 				youtubeUrl = new String(youtubeUrl.substring(0, youtubeUrl.indexOf("&list=")));
 			}
 //			Verifica se o id tem 11 caracteres
-			debug("11 caracteres");
-			if (youtubeUrl.contains("="))
+			debug("Verifica se tem 11 caracteres");
+			if (!youtubeUrl.contains("youtu.be/") && youtubeUrl.contains("="))
 			{
-				String id = new String(youtubeUrl.substring(youtubeUrl.indexOf("="), youtubeUrl.length()-1));
+				id = new String(youtubeUrl.substring(youtubeUrl.indexOf("="), youtubeUrl.length()-1));
 				if (id.length() != 11) 
 				{
 					System.out.println("Erro: id invalido!");
 					return;
 				}
 			}
-			
+			else if (youtubeUrl.contains("youtu.be/") && youtubeUrl.contains("?")) {
+				id = new String(youtubeUrl.substring(youtubeUrl.indexOf("be/")+3, youtubeUrl.indexOf("?")));
+//				System.out.println(id);
+				if (id.length() != 11) 
+				{
+					System.out.println("Erro: id invalido!");
+					return;
+				}
+			}
 			
 			ArrayList<String> result = new ArrayList<String>();
 			result = downloadYoutubeVideo(youtubeUrl, "");
@@ -86,11 +100,11 @@ public class Main {
 //			Verifica se o download ja foi feito anteriormente
 			else if (result.size()-2 > 0 && result.get(result.size()-2).contains("has already been downloaded"))
 			{
-				System.out.println("Ja foi feito o download!");
+				System.out.println("["+id+"] " + "Ja foi feito o download!");
 			}
 //			Se o comando for bem executado
 			else if (result.size()-1 > 0 && result.get(result.size()-1).compareTo("0") == 0) {
-				System.out.println("Transferencia executada com sucesso.");
+				System.out.println("["+id+"] " + "Transferencia executada com sucesso.");
 			}
 //			TODO Verificar outros erros
 			else {
@@ -100,6 +114,7 @@ public class Main {
 
 //		String format = new String("");
 //		getAllFormats(youtubeUrl);
+		System.out.println();
 	}
 
 	public static ArrayList<String> downloadYoutubeVideo(String youtubeUrl, String format) {
