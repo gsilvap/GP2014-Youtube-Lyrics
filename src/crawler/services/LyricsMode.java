d626eb4b262cf0a2ce1ef7153a203aad577bde84
celsomendes <cm.celsomendes@gmail.com>
2014-11-24 19:14:57 +0000

    LyricsMode Completo Falta alterar de autor/titulo separado para uma so string

--- src/crawler/services/LyricsMode.java
+++ src/crawler/services/LyricsMode.java
@@ -1,7 +1,5 @@
 package crawler.services;
 
-import java.util.ArrayList;
-
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -17,13 +15,17 @@ public class LyricsMode implements LyricSite {
 	//	TODO Cleaning
 	//	TODO remove prints and return lyrica
 	public String downloadLyric(String nameAuthor, String nameMusic, int debug) {
+		nameAuthor = Utilities.unAccent(nameAuthor).toLowerCase().replace("ft.", "feat");
+		nameMusic = Utilities.unAccent(nameMusic);
+		nameAuthor = nameAuthor.replaceAll("[\\]\\[(){},.;!?<>%]", "");
+//		nameMusic = nameMusic.replaceAll("[\\]\\[(){},.;!?<>%]", "");
+//		
 		String urlSearch = URL + Utilities.changeStringToSearch(nameAuthor) + "+" + Utilities.changeStringToSearch(nameMusic);
-
-		ArrayList<String> links = new ArrayList<String>();
-		String aux = "";
+		
+		nameAuthor = nameAuthor.toLowerCase().replace("the ", "");
+		
 		if (debug == 1)
 			System.out.println(urlSearch);
-		aux = nameAuthor + "\n" + nameMusic + "\n" + urlSearch;
 		Document doc = Utilities.getDoc(urlSearch);
 		if (doc != null) {
 			Elements lyrics = doc.select(resultsDiv);
@@ -33,25 +35,22 @@ public class LyricsMode implements LyricSite {
 				urlOfLyric = "http://www.lyricsmode.com" + urlOfLyric;
 				if (debug == 1)
 					System.out.println(urlOfLyric);
-				aux += "\n" + urlOfLyric;
 				if (urlOfLyric.contains(changeStringToURL(nameMusic)) && urlOfLyric.contains(changeStringToURL(nameAuthor))) {
 					Document lyric = Utilities.getDoc(urlOfLyric);
 					Utilities.sleep();
 
 					String lyrica = lyric.select(lyricDiv).text();
-					System.out.println(lyrica);
-					aux += "\n" + lyrica;
-					links.add(aux);
+//					System.out.println(lyrica);
+					System.out.println("OK");
 					return lyrica;
 				}
 			}
-			links.add(aux);
-			//FIXME:WRITE LIST TO FILE
 		}
+		System.out.println("ERROR");
 		return null;
 	}
 
 	public static String changeStringToURL(String msg) {
-		return msg.toLowerCase().replace("'", "").replace("& ", "").replace(" ", "_");
+		return msg.toLowerCase().replace("'", "").replace("& ", "").replace(" ", "_").replace("-", "_").replace(".", "");
 	}
 }
