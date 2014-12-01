b9b27eb7ff2feae470be9e18c1d95084a18317f4
celsomendes <cm.celsomendes@gmail.com>
2014-12-01 21:56:24 +0000

    Crawler encontra 34/50 e já vai buscar titulos e artistas corretos

--- src/crawler/services/LyricsMode.java
+++ src/crawler/services/LyricsMode.java
@@ -1,5 +1,8 @@
 package crawler.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -11,43 +14,63 @@ public class LyricsMode implements LyricSite {
 	private static String URL = "http://www.lyricsmode.com/search.php?search=";
 	private static String resultsDiv = "a.b.search_highlight";
 	private static String lyricDiv = "p#lyrics_text.ui-annotatable";
-
-	//	TODO Cleaning
-	//	TODO remove prints and return lyrica
+	private static String bandDiv = ".header-band-name";
+	private static String songDiv = ".header-song-name";
+	
+	
 	public String downloadLyric(String music, int debug) {
-//		String music = "James Arthur - Impossible (Lyrics)";
+		HashMap<String, String> replaceList = new HashMap<String, String>();
+		replaceList.put("ft.", "feat");
+		replaceList.put("-", " ");
+		replaceList.put("instrumental", "");
+		replaceList.put("karaoke", "");
+		replaceList.put("version", "");
+		replaceList.put("w/", "");
+		replaceList.put("official", "");
+		replaceList.put("video", "");
+		replaceList.put("download", "");
+		replaceList.put("youtube", "");
+		replaceList.put("in the style of", "");
+		replaceList.put("hd", " ");
+		replaceList.put("hq", " ");
+		replaceList.put("with lyrics", "");
+		replaceList.put("lyrics", "");
+		replaceList.put("on screen", "");
+		replaceList.put("   ", " ");
+		replaceList.put("  ", " ");
+		
+		music = Utilities.unAccent(music).toLowerCase();
 
-		music = Utilities.unAccent(music).toLowerCase().replace("ft.", "feat");
+		music = music.replaceAll("[\\]\\[(){},.;!?<>%+~<>*\"_]", "");
 		
-//		nameAuthor = Utilities.unAccent(nameAuthor).toLowerCase().replace("ft.", "feat");
-//		nameMusic = Utilities.unAccent(nameMusic);
-//		nameAuthor = nameAuthor.replaceAll("[\\]\\[(){},.;!?<>%]", "");
-		
-		music = music.replaceAll("[\\]\\[(){},.;!?<>%+~<>*\"]", "");
-		music = music.replace("_", "").replace("-", " ").replace("  ", " ").replace("instrumental","").replace("version","")
-				.replace("w/","").replace("official","").replace("video", "").replace("download", "").replace("youtube","")
-				.replace("in the style of","").replace(" hd "," ").replace(" hq ", " ");
-		music = music.replace("with lyrics","").replace("lyrics", "").replace("on screen","").replace("karaoke", "").replace("   ", " ").replace("  ", " ");
-
-		//		String urlSearch = URL + Utilities.changeStringToSearch(nameAuthor) + "+" + Utilities.changeStringToSearch(nameMusic);
-		
-		if (music.endsWith(" ")){
-			music = music.substring(0, music.length()-1);
+		for (Map.Entry<String, String> element : replaceList.entrySet()) {
+			music = music.replace(element.getKey(), element.getValue());
 		}
-		
+
+		String prevMusic = music;
+		while (!(music = music.replace("  ", " ")).equals(prevMusic)) {
+			prevMusic = music;
+			music = music.replace("  ", " ");
+		}
+
+		if (music.endsWith(" ")) {
+			music = music.substring(0, music.length() - 1);
+		}
+
+		if (music.startsWith(" ")) {
+			music = music.substring(1, music.length());
+		}
+
 		String urlSearch = URL + Utilities.changeStringToSearch(music);
-		
-//		System.out.println(urlSearch);
-		
+
 		music = music.toLowerCase().replace("the ", "");
-//		nameAuthor = nameAuthor.toLowerCase().replace("the ", "");
-		
+
 		if (debug == 1)
 			System.out.println(urlSearch);
-		
+
 		Document doc = Utilities.getDoc(urlSearch);
 		String[] words = music.split(" ");
-		
+
 		if (doc != null) {
 			Elements lyrics = doc.select(resultsDiv);
 			for (Element element : lyrics) {
@@ -55,34 +78,36 @@ public class LyricsMode implements LyricSite {
 				String urlOfLyric = element.select("a").attr("href");
 
 				urlOfLyric = "http://www.lyricsmode.com" + urlOfLyric;
-				
+
 				if (debug == 1)
 					System.out.println(urlOfLyric);
 
 				for (String str : words) {
-					if (urlOfLyric.contains(changeStringToURL(str))){
+					if (urlOfLyric.contains(changeStringToURL(str))) {
 						count++;
 					}
 				}
-				
-				
-				if ((count/words.length)>0.70) {
-//				if (urlOfLyric.contains(changeStringToURL(nameMusic)) && urlOfLyric.contains(changeStringToURL(nameAuthor))) {
+
+				if ((count / words.length) > 0.70) {
 					Document lyric = Utilities.getDoc(urlOfLyric);
 					Utilities.sleep();
-
+					
+					String bandName = lyric.select(bandDiv).text();
+					String songTitle = lyric.select(songDiv).text().replace(" lyrics", "");
 					String lyrica = lyric.select(lyricDiv).text();
-//					System.out.println(lyrica);
+					
+					System.out.println("Banda: "+bandName);
+					System.out.println("Titulo: "+songTitle);
 					System.out.println("OK");
 					return lyrica;
-				}else{
-					
-					System.out.println("Values:" + count + "+" + words.length + "+" + count/words.length);
-				
+				} else {
+
+					System.out.println("Values:" + count + "+" + words.length + "+" + count / words.length);
+
 				}
 			}
 		}
-		
+
 		System.out.println("ERROR");
 		return null;
 	}
